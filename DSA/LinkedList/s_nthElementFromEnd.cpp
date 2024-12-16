5b5dd4c708fa2f3d502bdbc619d13c447b0adfa2
Shubham Paitl <sp716490@gmail.com>
2024-12-16 06:42:03 +0000

    s_nthElementFromEnd.cpp

---
+++ DSA/LinkedList/s_nthElementFromEnd.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+struct LinkedList
+{
+	int _iData;
+	LinkedList* _next;
+	LinkedList(int value)
+	{
+		_iData = value;
+		_next = NULL;
+	}
+};
+
+LinkedList* InsertAtTheEnd(LinkedList* iHead, int iValue)
+{
+	LinkedList* newNode = new LinkedList(iValue);
+	if(iHead == NULL)
+	{
+		iHead = newNode;
+		return iHead;
+	}
+
+	LinkedList* tempNode = iHead;
+	while (NULL != tempNode->_next)
+	{
+		tempNode = tempNode->_next;
+	}
+
+	tempNode->_next = newNode;
+	return iHead;
+}
+
+void DisplayLinkedList(LinkedList* iHead)
+{
+	LinkedList* node = iHead;
+	while (NULL != node)
+	{
+		std::cout << node->_iData << " ";
+		node = node->_next;
+	}
+    std::cout << std::endl;
+}
+
+int CountLinkedListNodes(LinkedList* iHead)
+{
+    int count = 0;
+	LinkedList* node = iHead;
+	while (NULL != node)
+	{
+        ++count;
+		node = node->_next;
+	}
+    return count;
+}
+
+void PrintNthelementFromLast(LinkedList* iHead, int KthElement)
+{
+    int count = CountLinkedListNodes(iHead);
+    std::cout << "The totat nodes are " << count << std::endl;
+
+    if(KthElement > count) return;
+
+    int fromStart = count - KthElement + 1;
+    int startCount = 1;
+    LinkedList* curr = iHead;
+    while (startCount != fromStart)
+    {
+        curr = curr->_next;
+        ++startCount;
+    }
+    std::cout << "The " << KthElement << "th element from last is " << curr->_iData << std::endl;
+}
+
+int main()
+{
+	LinkedList* head = NULL;
+	head = InsertAtTheEnd(head, 1);
+	head = InsertAtTheEnd(head, 2);
+	head = InsertAtTheEnd(head, 3);
+	head = InsertAtTheEnd(head, 4);
+	head = InsertAtTheEnd(head, 5);
+	DisplayLinkedList(head);
+    PrintNthelementFromLast(head, 1);
+	std::cout << std::endl;
+	return 0;
+}
