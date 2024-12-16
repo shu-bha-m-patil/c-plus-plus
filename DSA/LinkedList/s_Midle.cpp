50d1d16547e0e764cba344c36001c70b820d247b
Shubham Paitl <sp716490@gmail.com>
2024-12-16 06:25:11 +0000

    Added Middle.cpp

--- DSA/LinkedList/s_Midle.cpp
+++ DSA/LinkedList/s_Midle.cpp
@@ -34,19 +34,15 @@ void PrintMidleNode(LinkedList* iHead)
 {
     if(iHead == nullptr) return;
 
-    if(iHead->_next == nullptr) 
-        std::cout << "The middle node is " << iHead->_iData << std::endl;
-
-
-	LinkedList* jumpNode = iHead;
-	LinkedList* curr = iHead;
-	while (jumpNode != nullptr && jumpNode->_next != nullptr)
+	LinkedList* fast = iHead;
+	LinkedList* slow = iHead;
+	while (fast != nullptr && fast->_next != nullptr)
 	{
-        curr = curr->_next;
-		jumpNode = jumpNode->_next->_next;
+        slow = slow->_next;
+		fast = fast->_next->_next;
 	}
 
-    std::cout << "The middle node is " << curr->_iData << std::endl;
+    std::cout << "The middle node is " << slow->_iData << std::endl;
 }
 
 void Display(LinkedList* iHead)
