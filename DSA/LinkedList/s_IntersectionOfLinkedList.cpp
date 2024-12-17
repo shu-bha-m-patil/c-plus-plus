57fcd2900d1ae1c4a795073bc3e318be148ab5be
shu-bha-m-patil <sp716490@gmail.com>
2024-12-17 08:37:28 +0530

    Added s_IntersectionOfLinkedList.cpp

--- DSA/LinkedList/s_IntersectionOfLinkedList.cpp
+++ DSA/LinkedList/s_IntersectionOfLinkedList.cpp
@@ -41,26 +41,10 @@ void Display(LinkedList* iHead)
     std::cout << std::endl;
 }
 
-void DeleteNode(LinkedList* iNode)
-{
-    if(iNode->_next == nullptr)
-    {
-        delete iNode;
-        iNode = nullptr;
-    }
-
-    int tempData = iNode->_iData;
-    iNode->_iData = iNode->_next->_iData;
-    iNode->_next->_iData = tempData;
-
-    LinkedList* tempNode = iNode->_next;
-    iNode->_next = iNode->_next->_next;
-
-    delete tempNode; tempNode = nullptr;
-}
-
 int main()
 {
+    // 1. Using unordered_set
+    // 2. Last linked list size - small liked list stize, travelling same speed
 	LinkedList* head = NULL;
 	head = InsertAtTheEnd(head, 1);
 	head = InsertAtTheEnd(head, 2);
@@ -68,17 +52,7 @@ int main()
 	head = InsertAtTheEnd(head, 4);
 	head = InsertAtTheEnd(head, 5);
 	head = InsertAtTheEnd(head, 6);
-	head = InsertAtTheEnd(head, 7);
-	head = InsertAtTheEnd(head, 8);
 	Display(head);
-
-    LinkedList* curr = head;
-    while(curr->_iData != 3)
-    {
-        curr = curr->_next;
-    }
-    DeleteNode(curr);
-
-	Display(head);
+    PrintIntersectionUsingHashTable();
 	return 0;
 }
