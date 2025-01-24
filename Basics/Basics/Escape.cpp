4774d79dcc15babc1ba4d6bfc0f039489988a600
shu-bha-m-patil <sp716490@gmail.com>
2025-01-24 15:24:31 +0530

    Removed Algorithms and Basics

--- Basics/Basics/Escape.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main()
-{
-    std::cout << "Welcome to \"geeksforgeeks\"" << std::endl;
-    std::cout << "Welcome to \\ geeksforgeeks" << std::endl;
-    std::cout << "c:\\temp\\spl88" << std::endl;
-    return 0;
-}
